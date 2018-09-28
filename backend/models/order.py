799309c02bf3e1f18680b1aa41aa43d8fee7fdc6
root <root@localhost.localdomain>
2018-09-28 18:21:49 +0800

    done

--- backend/models/order.py
+++ backend/models/order.py
@@ -9,4 +9,6 @@ class Order(CRUD,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.Integer,default = 0)  # 0     1     2
     shop_id = db.Column(db.VARCHAR(255))
+    ctime = db.Column(db.Integer)
+    utime = db.Column(db.Integer)
     order_num = db.Column(db.VARCHAR(255))
--- backend/services/user/user.py
+++ backend/services/user/user.py
@@ -4,8 +4,8 @@ from flask import g, request, jsonify, render_template
 from backend.common import query_from_argument, query_from_json, error_handler, Redisi
 from . import bp_user
 
-from time import time
-from datetime import datetime
+import time
+import datetime
 
 time_format = "%Y-%m-%d"
 
@@ -16,11 +16,11 @@ def user_init():
     print "%%%%%%%%%%%%%", g.phone, g.user_id, g.shop_id
     return jsonify({"re": "200", "msg": "success", "data": {}})
 
-@bp_user.route("/sign_in", methods=["POST", "GET"])
+@bp_user.route("/sign_in", methods=["POST"])
 @error_handler
 def sign_in():
     args = request.json
-    args = {}
+    # args = {}
     # args["phone"] = "18956537890"
     if args.get("phone"):
         shop = query_from_argument(Dining_shop, {"phone": args["phone"]}).first()
@@ -33,16 +33,15 @@ def sign_in():
         return jsonify({"re": "404", "msg": "phone_none", "data": {}})
     return jsonify({"re": "402", "msg": "param_error", "data": {}})
 
-@bp_user.route("/get_user_order", methods=["POST", "GET"])
+@bp_user.route("/get_user_order", methods=["POST"])
 @error_handler
 def get_user_order():
     data = []
-    now = datetime.fromtimestamp(time()).strftime(time_format)
+    now = datetime.datetime.fromtimestamp(time.time()).strftime(time_format)
+    print "###########now = ", now
     orders = query_from_argument(Dining_order, {"shop_id": g.shop_id})
-    print "###########$$$$$$$$$$$$$$$",orders.count()
     orders = orders.filter(Dining_order.status.in_(tuple([3,4])))
-    print "###########$$$$$$$$$$$$$$$",orders.count()
-    orders = orders.filter(Dining_order.pay_time.like("%" + "2018-09-25" + "%"))
+    orders = orders.filter(Dining_order.pay_time.like("%" + now + "%"))
     orders = orders.order_by(Dining_order.id.desc())
     orders = orders.all()
     if orders:
@@ -74,7 +73,8 @@ def get_user_order():
                     continue
             else:
                 order_local = Order()
-                order_local.init({"status": 0, "order_num": order.order_num, "shop_id":g.shop_id})
+                ctime = handler_time(order.add_time)
+                order_local.init({"status": 0, "order_num": order.order_num, "shop_id":g.shop_id, "ctime": ctime})
 
             # print "@@@@@@@@@@@@@", order.user_id, "^^^^^^^",g.shop_id
             # print "###############", goods
@@ -82,13 +82,20 @@ def get_user_order():
             # break
     return jsonify({"re": "200", "msg": "success", "data": data})
 
-@bp_user.route("/get_all_orders", methods=["POST","GET"])
+@bp_user.route("/get_all_orders", methods=["POST"])
 @error_handler
 def get_all_orders():
+    args = request.json
+    # args = {}
+    # args["startNum"] = "0"
+    # args["pageSize"] = "10"
     data = []
-    orders = query_from_argument(Dining_order, {"shop_id": g.shop_id, "status": 5})
-    # orders = orders.outerjoin(Order, Order.order_num == Dining_order.order_num)
-    orders = orders.order_by(Dining_order.id.desc())
+    total_num = 0
+    now = datetime.datetime.fromtimestamp(time.time()).strftime(time_format)
+    orders = query_from_argument(Dining_order, {"shop_id": g.shop_id}).filter(Dining_order.status.in_(tuple([3,4,5])))
+    orders = orders.filter(Dining_order.pay_time.like("%" + now + "%"))
+    total_num = orders.count()
+    orders = orders.order_by(Dining_order.id.desc()).offset(int(args["startNum"])).limit(args["pageSize"])
     orders = orders.all()
     if orders:
         for order in orders:
@@ -114,9 +121,9 @@ def get_all_orders():
             goods = eval(order.goods_list)
             info["goods"] = goods
             data.append(info)
-    return jsonify({"re": "200", "msg": "success", "data": data})
+    return jsonify({"re": "200", "msg": "success", "data": {"detail":data, "total_num": total_num}})
 
-@bp_user.route("/print_order", methods=["POST", "GET"])
+@bp_user.route("/print_order", methods=["POST"])
 @error_handler
 def print_order():
     args = request.json
@@ -125,3 +132,43 @@ def print_order():
     if order:
         order.change_data({"status": 1})
     return jsonify({"re": "200", "msg": "success", "data": {}})
+
+@bp_user.route("/get_printed_orders", methods=["POST"])
+@error_handler
+def get_printed_orders():
+    args = request.json
+    data = []
+    today = str(datetime.date.today()) + "  00:00:00"
+    today_ctime = handler_time(today)
+    local_orders = query_from_argument(Order, {"shop_id": g.shop_id, "status": 1}).filter(Order.ctime > today_ctime).all()
+    for local_order in local_orders:
+        order = query_from_argument(Dining_order, {"shop_id": g.shop_id, "order_num": local_order.order_num}).first()
+        if order:
+            info = {}
+            info["order_num"] = order.order_num
+            info["shop_name"] = order.shop_name
+            info["add_time"] = order.add_time
+            info["pay_time"] = order.pay_time
+            info["sender_phone"] = order.sender_phone
+            address_json = eval(order.address_json)
+            sender = query_from_argument(Dining_sender, {"id": order.sender_id}).first()
+            if sender:
+                info["sender_name"] = sender.true_name
+                info["sender_sex"] = ("male" if sender.sex ==1 else "female")
+            info["user_name"] = address_json.get("call_name")
+            info["user_phone"] = address_json.get("phone")
+            info["user_address"] = address_json.get("address")
+            info["message"] = order.message
+            info["fee"] = order.fee
+            info["person_count"] = order.person_count
+            info["shop_phone"] = order.shop_phone
+            info["total_box_fee"] = order.total_box_fee
+            goods = eval(order.goods_list)
+            info["goods"] = goods
+            data.append(info)
+    return jsonify({"re": "200", "msg": "success", "data": data})
+
+def handler_time(dt):
+    timeArray = time.strptime(dt, "%Y-%m-%d %H:%M:%S")
+    today_ctime = int(time.mktime(timeArray))
+    return today_ctime
